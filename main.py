3c82b6f60a95d852cffb37c58ed03a69aca57fe8
jakobeilts <jakobeilts@googlemail.com>
2025-05-07 18:03:49 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import llama_index_init
+from llama_index.core import SimpleDirectoryReader
+from llama_index.core import VectorStoreIndex
+
+if __name__ == "__main__":
+    llama_index_init.init()
+
+    documents = SimpleDirectoryReader("./data").load_data()
+    vector_index = VectorStoreIndex.from_documents(documents)
+    query_engine = vector_index.as_query_engine()
+
+    response = query_engine.query("Wer ist Datenschutzbeauftragter?")
+    print(response)
